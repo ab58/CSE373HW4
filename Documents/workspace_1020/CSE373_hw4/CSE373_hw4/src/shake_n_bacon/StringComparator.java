0eb050aecf323f4411be2b02e959075840638499
ab58 <arjunbhalla675@gmail.com>
2014-05-14 11:21:18 -0700

    Update StringComparator.java

--- Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringComparator.java
+++ Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringComparator.java
@@ -45,20 +45,29 @@ public class StringComparator implements Comparator<String> {
             shorter = s2;
             longer = s1;
         }
-        boolean returned = false;
-        for (int i=0; i<shorter.length()&&!returned; i++)
+        boolean different = false;
+        for (int i=0; i<shorter.length()&&!different; i++)
         {
             char cShort = shorter.charAt(i);
             char cLong = longer.charAt(i);
             if (cShort<cLong) {
-                returned = true;
-                return -1;
+                different = true;
+                if (shorter.length()==s1.length())
+                    return -1;
+                else
+                    return 1;
             } else if (cShort>cLong) {
-                returned = true;
-                return 1;
+                different = true;
+                if (shorter.length()==s1.length())
+                    return 1;
+                else
+                    return -1;
             } else if (i==shorter.length()-1&&longer.length()>shorter.length()) {
-                returned = true;
-                return 1;
+                different = true;
+                if (shorter.length()==s1.length())
+                    return -1;
+                else
+                    return 1;
             }
         }
         return 0;
