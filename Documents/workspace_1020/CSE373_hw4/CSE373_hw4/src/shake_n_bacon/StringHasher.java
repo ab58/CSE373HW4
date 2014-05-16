1f81b9b7ff8e8aaf10374fc4801ece41daa959be
Patrick Harper-Joles <hattrick042@gmail.com>
2014-05-15 17:43:09 -0700

    Update StringHasher.java

--- Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringHasher.java
+++ Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringHasher.java
@@ -3,26 +3,20 @@ package shake_n_bacon;
 import providedCode.Hasher;
 
 /**
- * @author <name>
- * @UWNetID <uw net id>
- * @studentID <id number>
- * @email <email address>
+* Patrick Harper-Joles / Arjun Bhalla
+ * hatrik42 / ab58
+ * 1440683 / 1363119
+ * hatrik42@uw.edu / arjunbhalla675@gmail.com
  */
 public class StringHasher implements Hasher {
-
-    HashTable_SC.Bucket[] buckets;
-	/**
-	 * TODO Replace this comment with your own as appropriate.
-	 */
-	@Override
-	public int hash(String str) {
-
-		char[] cArr = str.toLowerCase().toCharArray();
-        int sum = 0;
-        for (int i=0; i<cArr.length; i++)
-        {
-            sum+=cArr[i];
-        }
-		return sum%buckets.length;
-	}
+   
+   //
+   public int hash(String str) {
+      char[] cArr = str.toLowerCase().toCharArray();
+      int sum = 0;
+      for (int i = 0; i < cArr.length; i++) {
+         sum += cArr[i];
+      }
+      return sum;
+   }
 }
