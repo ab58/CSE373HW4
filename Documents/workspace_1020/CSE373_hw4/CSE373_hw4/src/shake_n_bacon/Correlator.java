f1bb854af268505c0b536b604b2521fdadf3ae75
Patrick Harper-Joles <hattrick042@gmail.com>
2014-05-16 16:55:43 -0700

    Update Correlator.java

--- Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/Correlator.java
+++ Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/Correlator.java
@@ -30,11 +30,115 @@ import providedCode.*;
  *        and change it there as needed for your write-up experiments.
  */
 public class Correlator {
-
+   double variance = 0.0;
+   int firstTotal = 0;
+   int secondTotal = 0;
+   
 	public static void main(String[] args) {
-		// TODO: Compute this variance
-		double variance = 0.0;
-		// IMPORTANT: Do not change printing format. Just print the double.
+		      
+      if (args.length != 2) {
+			usage();
+		}
+
+		String firstArg = args[0].toLowerCase();
+		DataCounter counter = null;
+		if (firstArg.equals("-s")) {
+			counter = new HashTable_SC(new StringComparator(),
+					new StringHasher());
+		} else if (firstArg.equals("-o")) {
+			counter = new HashTable_OA(new StringComparator(),
+					new StringHasher());
+		} else {
+			usage();
+		}
+
+		countWords(args[1], counter);
+		DataCount[] counts1 = getCountsArray(counter);
+		insertionSort(counts1, new DataCountStringComparator());
+      firstTotal = totalWords(counts1);
+      
+      countWords(args[1], counter);
+		DataCount[] counts2 = getCountsArray(counter);
+		insertionSort(counts2, new DataCountStringComparator());
+      secondTotal = totalWords(counts2);
+      
+      
 		System.out.println(variance);
 	}
+   
+   private static void countWords(String file, DataCounter counter) {
+		try {
+			FileWordReader reader = new FileWordReader(file);
+			String word = reader.nextWord();
+			while (word != null) {
+				counter.incCount(word);
+				word = reader.nextWord();
+			}
+		} catch (IOException e) {
+			System.err.println("Error processing " + file + " " + e);
+			System.exit(1);
+		}
+	}
+   
+   private static <E> void insertionSort(E[] array, Comparator<E> comparator) {
+		for (int i = 1; i < array.length; i++) {
+			E x = array[i];
+			int j;
+			for (j = i - 1; j >= 0; j--) {
+				if (comparator.compare(x, array[j]) >= 0) {
+					break;
+				}
+				array[j + 1] = array[j];
+			}
+			array[j + 1] = x;
+		}
+	}
+
+	/*
+	 * Print error message and exit
+	 */
+	private static void usage() {
+		System.err
+				.println("Usage: [-s | -o] <filename of document to analyze>");
+		System.err.println("-s for hashtable with separate chaining");
+		System.err.println("-o for hashtable with open addressing");
+		System.exit(1);
+	}
+   
+   //
+   private static int totalWords (DataCount[] counts) {
+      int total = 0;
+      for (int i = 0; i < counts.length; i++) {
+         total = total + counts[i].count;
+      }
+      return total;
+   }
+   
+   //
+   private static double comparison (DataCount[] counts1, DataCount[] counts2, int total) {
+      int limit1 = counts1.length;
+      int limit2 = counts2.length;
+      double totalVar = 0;
+      
+      for (int i = 0; i < limit1; i++) {
+         
+         int cur1 = counts1[i].count;
+         double var1 = cur1 / total;
+         if (var1 < 0.01 && var1 > 0.0001) {
+            
+            for (int j = 0; j < limit2; j++) {
+               
+               if (counts2[j].data == counts1.data) {
+                  
+                  int cur2 = counts2[i].count;
+                  double var2 = cur2 / total;
+                  if (var2 < 0.01 && var2 > 0.0001) {
+                     compVar = Math.abs(var1 - var2);
+                     totalvar = totalVar + compVar;
+                  }
+               }
+            }
+         }         
+      }
+   }
 }
