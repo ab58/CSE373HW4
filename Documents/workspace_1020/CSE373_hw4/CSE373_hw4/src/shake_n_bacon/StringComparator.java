c5c158b039ab552851c1286203e0d177c7d4d7e4
Patrick Harper-Joles <hattrick042@gmail.com>
2014-05-15 17:52:53 -0700

    Update StringComparator.java

--- Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringComparator.java
+++ Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringComparator.java
@@ -3,10 +3,10 @@ package shake_n_bacon;
 import providedCode.*;
 
 /**
- * @author <name>
- * @UWNetID <uw net id>
- * @studentID <id number>
- * @email <email address>
+ * Patrick Harper-Joles / Arjun Bhalla
+ * hatrik42 / ab58
+ * 1440683 / 1363119
+ * hatrik42@uw.edu / arjunbhalla675@gmail.com
  * 
  *        TODO: REPLACE this comment with your own as appropriate.
  * 
@@ -28,48 +28,51 @@ import providedCode.*;
  *        their return values should have the same sign (+,- or 0).
  */
 public class StringComparator implements Comparator<String> {
-
-	/**
-	 * TODO Replace this comment with your own as appropriate.
-	 */
-	@Override
-	public int compare(String s1, String s2) {
-		// TODO: To-be implemented
-        String shorter = null;
-        String longer = null;
-        int shorterLength = Math.min(s1.length(),s2.length());
-        if (s1.length()==shorterLength) {
-            shorter = s1;
-            longer = s2;
-        } else {
-            shorter = s2;
-            longer = s1;
-        }
-        boolean different = false;
-        for (int i=0; i<shorter.length()&&!different; i++)
-        {
-            char cShort = shorter.charAt(i);
-            char cLong = longer.charAt(i);
-            if (cShort<cLong) {
-                different = true;
-                if (shorter.length()==s1.length())
-                    return -1;
-                else
-                    return 1;
-            } else if (cShort>cLong) {
-                different = true;
-                if (shorter.length()==s1.length())
-                    return 1;
-                else
-                    return -1;
-            } else if (i==shorter.length()-1&&longer.length()>shorter.length()) {
-                different = true;
-                if (shorter.length()==s1.length())
-                    return -1;
-                else
-                    return 1;
-            }
-        }
-        return 0;
-	}
+   
+   /**
+   * TODO Replace this comment with your own as appropriate.
+   */
+   @Override
+   public int compare(String s1, String s2) {
+        
+      // TODO: To-be implemented
+      String shorter = null;
+      String longer = null;
+      int shorterLength = Math.min(s1.length(),s2.length());
+      if (s1.length()==shorterLength) {
+         shorter = s1;
+         longer = s2;
+      } 
+      else {
+         shorter = s2;
+         longer = s1;
+      }
+      boolean different = false;
+      for (int i=0; i<shorter.length()&&!different; i++) {
+         char cShort = shorter.charAt(i);
+         char cLong = longer.charAt(i);
+         if (cShort<cLong) {
+            different = true;
+            if (shorter.length()==s1.length())
+               return -1;
+            else
+               return 1;
+         } 
+         else if (cShort>cLong) {
+            different = true;
+            if (shorter.length()==s1.length())
+               return 1;
+            else
+               return -1;
+            } 
+         else if (i==shorter.length()-1&&longer.length()>shorter.length()) {
+            different = true;
+            if (shorter.length()==s1.length())
+               return -1;
+            else
+               return 1;
+         }
+      }
+      return 0;
+   }
 }
