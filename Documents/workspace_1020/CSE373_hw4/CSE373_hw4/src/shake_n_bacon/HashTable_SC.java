270790b06e8d3d181254e34af8d134b4900a0e01
Patrick Harper-Joles <hattrick042@gmail.com>
2014-05-15 17:54:00 -0700

    Update HashTable_SC.java

--- Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/HashTable_SC.java
+++ Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/HashTable_SC.java
@@ -3,10 +3,10 @@ package shake_n_bacon;
 import providedCode.*;
 
 /**
- * @author <name>
- * @UWNetID <uw net id>
- * @studentID <id number>
- * @email <email address>
+ * Patrick Harper-Joles / Arjun Bhalla
+ * hatrik42 / ab58
+ * 1440683 / 1363119
+ * hatrik42@uw.edu / arjunbhalla675@gmail.com
  * 
  *        TODO: Replace this comment with your own as appropriate.
  * 
