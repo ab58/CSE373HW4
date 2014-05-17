83b655e67b9feefc9e0b58925dd1d551d6bfd7f7
Patrick Harper-Joles <hattrick042@gmail.com>
2014-05-17 14:44:14 -0700

    Update StringHasher.java

--- Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringHasher.java
+++ Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringHasher.java
@@ -2,20 +2,20 @@ package shake_n_bacon;
 
 import providedCode.Hasher;
 
-/**
-* Patrick Harper-Joles / Arjun Bhalla
- * hatrik42 / ab58
- * 1440683 / 1363119
- * hatrik42@uw.edu / arjunbhalla675@gmail.com
- */
+// Patrick Harper-Joles / Arjun Bhalla
+// hatrik42 / ab58
+// 1440683 / 1363119
+// hatrik42@uw.edu / arjunbhalla675@gmail.com
+
+//
+
 public class StringHasher implements Hasher {
    
    //
    public int hash(String str) {
-      char[] cArr = str.toLowerCase().toCharArray();
       int sum = 0;
-      for (int i = 0; i < cArr.length; i++) {
-         sum += cArr[i];
+      for (int i = 0; i < str.length(); i++) {
+         sum += str.charAt(i);;
       }
       return sum;
    }
