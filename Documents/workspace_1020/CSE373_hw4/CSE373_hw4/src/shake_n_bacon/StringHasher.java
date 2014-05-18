7ea91fc6e7afe1bc8856d5682befd9b38db29c6c
Patrick Harper-Joles <hattrick042@gmail.com>
2014-05-17 17:04:46 -0700

    Update StringHasher.java

--- Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringHasher.java
+++ Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/StringHasher.java
@@ -7,11 +7,12 @@ import providedCode.Hasher;
 // 1440683 / 1363119
 // hatrik42@uw.edu / arjunbhalla675@gmail.com
 
-//
+// This class breaks a String into char ASCII values and adds them
+// to a running total for return.
 
 public class StringHasher implements Hasher {
    
-   //
+   // Returns the value of all the given String value's characters.
    public int hash(String str) {
       int sum = 0;
       for (int i = 0; i < str.length(); i++) {
