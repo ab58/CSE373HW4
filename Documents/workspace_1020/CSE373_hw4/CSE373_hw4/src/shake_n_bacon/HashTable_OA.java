996d88063e892d673ab1f98a8e1447d8ae531762
Patrick Harper-Joles <hattrick042@gmail.com>
2014-05-17 17:02:43 -0700

    Update HashTable_OA.java

--- Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/HashTable_OA.java
+++ Documents/workspace_1020/CSE373_hw4/CSE373_hw4/src/shake_n_bacon/HashTable_OA.java
@@ -7,19 +7,20 @@ import providedCode.*;
 // 1440683 / 1363119
 // hatrik42@uw.edu / arjunbhalla675@gmail.com
 
-//
+// This class creates a HashTable using the Linear Probing model.
 
 public class HashTable_OA extends DataCounter {
    private DataCount[] arrayOA;
    private Hasher ha;
    
-   //
+   // Constructs new HashTable_OA objects.
 	public HashTable_OA(Comparator<String> c, Hasher h) {
 		arrayOA = new DataCount[17];
       ha = h; 
 	}
 
-	//
+	// Creates DataCount nodes, hashes the given string value, and inserts
+   // the DataCount node into the HashTable at the hashed value index.
 	public void incCount(String data) {
 		if (getSize() >= arrayOA.length / 2) {
          resize();
@@ -27,7 +28,12 @@ public class HashTable_OA extends DataCounter {
       int key = ha.hash(data);
       key = key % arrayOA.length;
       while (arrayOA[key] != null && arrayOA[key].data != data) {
-         key++;
+         if (key == arrayOA.length - 1) {
+            key = 0;
+         }
+         else {
+            key++;
+         }
       }
       if (arrayOA[key] == null) {
          DataCount temp = new DataCount(data, 1);
@@ -38,7 +44,7 @@ public class HashTable_OA extends DataCounter {
       }
 	}
 
-	//
+	// Returns the number of unique DataCount nodes in the HashTable.
 	public int getSize() {
 		int size = 0;
       for (int i = 0; i < arrayOA.length; i++) {
@@ -51,7 +57,7 @@ public class HashTable_OA extends DataCounter {
 		return size;
 	}
 
-	//
+	// Returns the count in the HashTable of the given DataCount data value.
 	public int getCount(String data) {
 	   int key = ha.hash(data);
       while (arrayOA[key].data != data) {
@@ -64,12 +70,12 @@ public class HashTable_OA extends DataCounter {
         
 	}
 
-   // 
+   // Creates a class iterator used to look through HashTable DataCount nodes.
 	public SimpleIterator getIterator() {
       return new SimpleIterator() {
          int cur = 0;
          
-         //
+         // Returns true if the next DataCount node exists.
          public boolean hasNext() {
             if (cur == 0 && arrayOA[cur] != null) {
                return true; 
@@ -82,7 +88,7 @@ public class HashTable_OA extends DataCounter {
             }
          }
          
-         //
+         // Returns the next DataCount node.
          public DataCount next() {
             if (hasNext()) {
                DataCount next = arrayOA[cur];
@@ -95,7 +101,7 @@ public class HashTable_OA extends DataCounter {
       };
    }
    
-   //
+   // Searches for empty indexes within HashTable.
    public int searcher (int key) {
       if (arrayOA[key] != null) {
          searcher(key + 1);
@@ -106,19 +112,30 @@ public class HashTable_OA extends DataCounter {
       return -1;
    }
    
-   //
+   // Resizes the current HashTable.
    public void resize () {
       DataCount[] temp = new DataCount[maxPrimeUnderDouble(arrayOA.length)];
       for (int i = 0; i < arrayOA.length; i++) {
          if (arrayOA[i] != null) {
-            int x = ha.hash(arrayOA[i].data);
-            temp[x] = arrayOA[i];
+            int key = ha.hash(arrayOA[i].data);
+            key = key % temp.length;
+            while (temp[key] != null) {
+               if (key == temp.length - 1) {
+                  key = 0;
+               }
+               else {
+                  key++;
+               }
+            }
+            if (temp[key] == null) {
+               temp[key] = arrayOA[i];                 
+            }
          }
       }
       arrayOA = temp;
    }
    
-   //
+   // Returns the highest prime number under double the current integer value.
    private int maxPrimeUnderDouble(int x) {
       int y = x;
       x = x * 2;
@@ -138,16 +155,19 @@ public class HashTable_OA extends DataCounter {
       return y;
    }
    
-   //
+   // Searches the current HashTable for duplicate DataCount data values.
    private boolean unique (String cur, int x) {
       for (int i = 0; i < arrayOA.length; i++) {
          if (i == x) {
-          
+            i++;
          }
-         else if (arrayOA[i].data == cur) {
+         else if (arrayOA[i] != null && arrayOA[i].data == cur) {
             return false;
          }
+         else {
+            return true;
+         }
       }
-      return true;
+      return false;
    }
 }
